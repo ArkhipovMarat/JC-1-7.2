353b433f63509145610a5de224d7b58dfa74a9ad
MaratArkhipov <maratkzn2010@yandex.ru>
2020-08-08 00:40:17 +0400

    all done

---
+++ src/test/java/ArrayListStreamTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class ArrayListStreamTest {
+    @Test
+    void streamTest () {
+    List<Integer> arrayListInput = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+    List<Integer> arrayListResult = Arrays.asList(2, 4, 8, 16, 32);
+
+    Stream<Integer> stream = arrayListInput.stream();
+    List<Integer> arrayListStream = stream
+            .filter(x -> x > 0)
+            .filter(x -> (x % 2)==0)
+            .sorted(Comparator.naturalOrder())
+            .collect(Collectors.toList());
+
+    Assertions.assertEquals(arrayListResult,arrayListStream);
+    }
+}
