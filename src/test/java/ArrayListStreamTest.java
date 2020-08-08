8e60cef86719a703def07a63ebf01006fb26932b
MaratArkhipov <maratkzn2010@yandex.ru>
2020-08-08 20:17:32 +0400

    added Hamcrest-test

--- src/test/java/ArrayListStreamTest.java
+++ src/test/java/ArrayListStreamTest.java
@@ -5,13 +5,14 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class ArrayListStreamTest {
     @Test
     void streamTest () {
     List<Integer> arrayListInput = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-    List<Integer> arrayListResult = Arrays.asList(2, 4, 8, 16, 32);
+//    List<Integer> arrayListResult = Arrays.asList(2, 4, 8, 16, 32);
 
     Stream<Integer> stream = arrayListInput.stream();
     List<Integer> arrayListStream = stream
@@ -20,6 +21,8 @@ public class ArrayListStreamTest {
             .sorted(Comparator.naturalOrder())
             .collect(Collectors.toList());
 
-    Assertions.assertEquals(arrayListResult,arrayListStream);
+//    Assertions.assertEquals(arrayListResult,arrayListStream);
+
+    assertThat(arrayListStream, contains(2, 4, 8, 16, 32));
     }
 }
